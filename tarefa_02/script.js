7a207e9f43c27f716c177609902064a9452732db
Gustavo <gstvfukui@gmail.com>
2026-08-18 21:47:43 -0300

    Added js

---
+++ tarefa_02/script.js
@@ -0,0 +1,14 @@
+let contador = 0
+
+document.getElementById("Botao").onclick = () => {
+    contador++;
+    let toast = document.getElementById("toast");
+    if (!toast) {
+        toast = document.createElement("div");
+        toast.id = "toast";
+        toast.className = "toast";
+        document.body.appendChild(toast);
+    }
+    toast.textContent = `Você clicou ${contador} vezes!`;
+    toast.classList.add("show");
+}
